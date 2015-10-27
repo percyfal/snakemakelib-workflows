44872731d7aea1e216cd436211c85a055d1a9a64
Per Unneberg <per.unneberg@scilifelab.se>
2015-10-28 00:25:40 +0100

    pca plots work again

---
+++ snakemakelib_workflows/scrnaseq/scrnaseq.py
@@ -0,0 +1,251 @@
+# Copyright (C) 2015 by Per Unneberg
+import math
+import pandas as pd
+import jinja2
+import numpy as np
+import pickle
+from snakemake.report import data_uri
+from snakemakelib.odo.utils import annotate_df
+from snakemakelib.odo import star, rseqc
+from snakemakelib import SNAKEMAKELIB_TEMPLATES_PATH
+from snakemakelib.resources import css_files, js_files
+from snakemakelib.plot.bokeh import static_html
+from snakemakelib_workflows import SNAKEMAKELIB_WORKFLOWS_TEMPLATES_PATH
+from bokeh.models import ColumnDataSource, TableColumn, DataTable, HoverTool, BoxSelectTool
+from bokeh.plotting import figure, gridplot, output_file, show
+from bokehutils.axes import xaxis, yaxis
+from bokehutils.geom import dotplot, points
+from bokehutils.tools import tooltips
+from snakemakelib.plot.sklearn import plot_pca
+from snakemakelib.statistics import pca, pca_results
+from snakemakelib.applications.rnaseq import  number_of_detected_genes
+
+DEFAULT_TOOLS = "pan,wheel_zoom,box_zoom,box_select,lasso_select,resize,reset,save,hover"
+
+# Template environment
+loader = jinja2.FileSystemLoader([SNAKEMAKELIB_TEMPLATES_PATH,
+                                  SNAKEMAKELIB_WORKFLOWS_TEMPLATES_PATH])
+Env = jinja2.Environment(loader=loader)
+
+def _collect_alignment_metrics(targets, parser):
+    """Collect alignment metrics"""
+    def _add_rows(df):
+        df.loc['mismatch_sum', :] = df.loc['Mismatch rate per base, %',:].copy()
+        df.loc['mismatch_sum', "value"] = df.loc['Mismatch rate per base, %', "value"]   + df.loc['Deletion rate per base', "value"] + df.loc['Insertion rate per base', "value"]
+        df.loc['% of reads unmapped', :] = df.loc['% of reads unmapped: other', :]
+        df.loc['% of reads unmapped', "value"] = df.loc['% of reads unmapped: other', "value"] + df.loc['% of reads unmapped: too many mismatches', "value"] + df.loc['% of reads unmapped: too short', "value"]
+        return df
+        
+    dflist = [_add_rows(annotate_df(t, parser, groupnames=["SM", "PU"])) for t in targets]
+    df_long = pd.concat(dflist)
+    return df_long
+
+def _collect_rseqc_metrics(read_distribution, gene_body_coverage, parser):
+    """Collect rseqc metrics"""
+    def _add_gbc_rows(df):
+        df['three_prime_map'] = 100.0 * df.loc[:, "91":"100"].sum(axis=1) / df.loc[:, "1":"100"].sum(axis=1)
+        return df
+    def _add_rd_rows(df):
+        ExonMap = df.loc['Introns', :]
+        cols = ["Total_bases", "Tag_count", "Tags/Kb"]
+        ExonMap.loc[cols] = 100 * (df.loc['CDS_Exons', cols] + df.loc["3'UTR_Exons", cols] + df.loc["5'UTR_Exons", cols]) / df.loc[:, cols].sum(axis=0)
+        df.loc["ExonMap_%", :] = ExonMap
+        return df
+    gbc = pd.concat([_add_gbc_rows(annotate_df(f, parser)) for f in gene_body_coverage])
+    gbc = gbc.set_index(gbc["SM"])
+    rd = pd.concat([_add_rd_rows(annotate_df(f, parser)) for f in read_distribution])
+    return {"read_distribution": rd, "gene_body_coverage": gbc}
+
+
+def _dotplot(**kwargs):
+    """Make a dotplot figure. 
+
+    FIXME: still depends on bokehutils
+    """
+    kwfig = kwargs.pop('kwfig', {})
+    kwxaxis = kwargs.pop('kwxaxis', {})
+    kwyaxis = kwargs.pop('kwyaxis', {})
+    fig = figure(title=kwargs.get('title', "Dotplot"),
+                 tools=kwargs.get('tools', DEFAULT_TOOLS),
+                 **kwfig)
+    source = (kwargs.get('source'))
+    dotplot(fig, kwargs['x'], kwargs['y'], source=kwargs.get('source', None))
+    xaxis(fig, **kwxaxis)
+    yaxis(fig, **kwyaxis)
+    return fig
+
+def _align_rseqc_plot(align_metrics, rseqc_metrics, outfile):
+    df = align_metrics.drop(align_metrics.index[[0,1,2]]).reset_index()
+    df["value"] = pd.to_numeric(df["value"])
+    df = df.pivot_table(values="value", index="SM", columns="name")
+    df = df.join(rseqc_metrics["read_distribution"].reset_index().pivot_table(columns="Group", values="Tag_count", index="SM"))
+    df = df.join(rseqc_metrics["gene_body_coverage"])
+    # Remember: ColumnDataSource column names cannot include spaces or %
+    df.columns = [x.replace(" ", "_").replace("%", "PCT") for x in df.columns]
+    df.to_csv(outfile)
+    source = ColumnDataSource(df)
+    columns = [
+        TableColumn(field="SM", title="Sample"),
+        TableColumn(field="Number_of_input_reads",
+                    title="Number of input reads"),
+        TableColumn(field="Uniquely_mapped_reads_PCT",
+                    title="Uniquely mapped reads (%)"),
+        TableColumn(field="Mismatch_rate_per_base,_PCT",
+                    title="Mismatch rate per base (%)"),
+        TableColumn(field="Insertion_rate_per_base",
+                    title="Insertion rate per base (%)"),
+        TableColumn(field="Deletion_rate_per_base",
+                    title="Deletion rate per base (%)"),
+        TableColumn(field="PCT_of_reads_unmapped",
+                    title="Unmapped reads (%)"),
+    ]
+    table = DataTable(source=source, columns=columns,
+                      editable=False, width=1000)
+    kwfig = {'plot_width': 400, 'plot_height': 400, 
+             'title_text_font_size': "16pt"}
+    kwxaxis = {'axis_label': 'Sample',
+               'axis_label_text_font_size': '12pt',
+               'major_label_orientation': np.pi/3}
+    kwyaxis = {'axis_label_text_font_size': '12pt',
+               'major_label_orientation': np.pi/3}
+
+    # Input reads
+    p1 = _dotplot(
+        kwfig=dict(kwfig, **{'x_range': list(df.index), 'y_axis_type': "log"}),
+        kwxaxis=kwxaxis,
+        kwyaxis=dict(kwyaxis, **{'axis_label': "Reads"}),
+        title="Number of input reads", tools=DEFAULT_TOOLS, 
+        x="SM", y="Number_of_input_reads", source=source
+    )
+    tooltips(p1, HoverTool, [('Sample', '@SM'),
+                             ('Reads', '@Number_of_input_reads')])
+
+    # Uniquely mapping
+    p2 = _dotplot(
+        kwfig=dict(kwfig, **{'x_range': p1.x_range, 'y_range': [0, 100]}),
+        kwxaxis=kwxaxis,
+        kwyaxis=dict(kwyaxis, **{'axis_label': 'Percent'}),
+        tools=DEFAULT_TOOLS, title="Uniquely mapping reads",
+        x="SM", y="Uniquely_mapped_reads_PCT", source=source
+    )
+    tooltips(p2, HoverTool, [('Sample', '@SM'),
+                             ('Pct_mapped', '@Uniquely_mapped_reads_PCT')])
+
+    # Unmapped
+    p3 = _dotplot(
+        kwfig=dict(kwfig, **{'x_range': p1.x_range, 'y_range': [0, 100]}),
+        kwxaxis=kwxaxis,
+        kwyaxis=dict(kwyaxis, **{'axis_label': 'Percent'}),
+        tools=DEFAULT_TOOLS, title="Unmapped reads",
+        x="SM", y="PCT_of_reads_unmapped", source=source
+    )
+    tooltips(p3, HoverTool, [('Sample', '@SM'),
+                             ('Pct_unmapped', '@PCT_of_reads_unmapped')])
+
+    # Mismatch/indel rate
+    p4 = figure(title="Mismatch and indel rates",
+                x_range=p1.x_range,
+                tools=DEFAULT_TOOLS,
+                **kwfig)
+    dotplot(p4, "SM", "Mismatch_rate_per_base,_PCT", source=source, legend="Mismatch")
+    dotplot(p4, "SM", "Insertion_rate_per_base", source=source, legend="Insertion", color="red")
+    dotplot(p4, "SM", "Deletion_rate_per_base", source=source, legend="Deletion", color="green")
+    xaxis(p4, **kwxaxis)
+    yaxis(p4, axis_label="Percent", **kwyaxis)
+    tooltips(p4, HoverTool,  [('Sample', '@SM'),
+                              ('Mismatch rate per base',
+                               '@Mismatch_rate_per_base,_PCT'),
+                              ('Insertion rate per base',
+                               '@Insertion_rate_per_base'),
+                              ('Deletion rate per base',
+                               '@Deletion_rate_per_base'), ])
+    select_tool = p4.select(dict(type=BoxSelectTool))
+    select_tool.dimensions = ['width']
+    
+    # Unmapped
+    p5 = _dotplot(
+        kwfig=dict(kwfig, **{'x_range': p1.x_range}),
+        kwxaxis=kwxaxis,
+        kwyaxis=dict(kwyaxis, **{'axis_label': 'Percent'}),
+        tools=DEFAULT_TOOLS, title="Mismatch/indel sum",
+        x="SM", y="mismatch_sum", source=source
+    )        
+    tooltips(p5, HoverTool, [('Sample', '@SM'),
+                             ('Mismatch/indel rate per base',
+                              '@mismatch_sum'), ])
+    select_tool = p5.select(dict(type=BoxSelectTool))
+    select_tool.dimensions = ['width']
+
+    # Fraction reads mapping to 10% right-most end
+    p6 = _dotplot(
+        kwfig=dict(kwfig, **{'x_range': p1.x_range}),
+        kwxaxis=kwxaxis,
+        kwyaxis=dict(kwyaxis, **{'axis_label': 'Percent'}),
+        tools=DEFAULT_TOOLS, title="Tags mapping to exons",
+        x="SM", y="ExonMap_PCT", source=source
+    )
+    tooltips(p6, HoverTool, [('Sample', '@SM'),
+                             ('ExonMap (%)', '@ExonMap_PCT'), ])
+
+    # Fraction reads mapping to 10% right-most end
+    p7 = _dotplot(
+        kwfig=dict(kwfig, **{'x_range': p1.x_range}),
+        kwxaxis=kwxaxis,
+        kwyaxis=dict(kwyaxis, **{'axis_label': 'Percent'}),
+        tools=DEFAULT_TOOLS, title="Reads mapping to 3' end",
+        x="SM", y="three_prime_map", source=source
+    )
+    tooltips(p7, HoverTool, [('Sample', '@SM'),
+                             ("3' map", '@three_prime_map'), ])
+
+    return {'fig': gridplot([[p1, p2, p3], [p4, p5, p6], [p7]]), 'table': table}
+
+def scrnaseq_qc(config, input, output):
+    """Do QC of scrnaseq"""
+    align_metrics = _collect_alignment_metrics(
+        input.align_log,
+        config['settings']['sample_organization'].run_id_re
+    )
+    rseqc_metrics = _collect_rseqc_metrics(
+        input.rseqc_read_distribution,
+        input.rseqc_gene_body_coverage,
+        config['settings']['sample_organization'].sample_re
+    )
+    
+    # Alignment stats
+    d = {'align': _align_rseqc_plot(align_metrics, rseqc_metrics, output.alignqc)}
+
+    # rsem plots
+    if input.rsemgenes:
+        d.update({'rsem': plot_pca(input.rsemgenespca,
+                                  config['workflows.bio.scrnaseq']['metadata'],
+                                   input.rsemgenespca.replace(".pca.csv", ".pcaobj.pickle"))})
+
+    # rpkmforgenes plots
+    if input.rpkmforgenes:
+        d.update({'rpkmforgenes': plot_pca(input.rpkmforgenespca,
+                                  config['workflows.bio.scrnaseq']['metadata'],
+                                   input.rpkmforgenespca.replace(".pca.csv", ".pcaobj.pickle"))})
+
+    
+    d.update({'version' : config['_version'], 'config' : {'uri' : data_uri(input.globalconf), 'file' : input.globalconf}})
+    tp = Env.get_template('workflow_scrnaseq_qc.html')
+    with open(output.html, "w") as fh:
+        fh.write(static_html(tp, template_variables=d, css_raw=css_files, js_raw=js_files))
+
+
+def scrnaseq_pca(config, input, output):
+    expr_long = pd.read_csv(input.expr)
+    expr_long["TPM"] = [math.log2(x+1.0) for x in expr_long["TPM"]]
+    expr = expr_long.pivot_table(columns="gene_id", values="TPM",
+                                 index="SM")
+    detected_genes = number_of_detected_genes(input.expr)
+    pcaobj = pca(expr)
+    pcares = pca_results(pcaobj, expr, metadata=config['workflows.bio.scrnaseq']['metadata'])
+    if not detected_genes is None:
+        pcares = pcares.join(detected_genes)
+    with open(output.pca, "w") as fh:
+        pcares.to_csv(fh)
+    with open(output.pcaobj, "wb") as fh:
+        pickle.dump(pcaobj, fh)
+
--- snakemakelib_workflows/scrnaseq/scrnaseq.workflow
+++ snakemakelib_workflows/scrnaseq/scrnaseq.workflow
@@ -1,1 +1,210 @@
 # -*- snakemake -*-
+import shutil
+import os
+import math
+from os.path import join, dirname, relpath, exists
+import pickle
+from snakemake.utils import update_config, set_temporary_output, set_protected_output
+from snakemake.workflow import workflow
+from snakemake_rules import SNAKEMAKE_RULES_PATH
+from snakemakelib.targets import make_targets
+from snakemakelib.sample.input import initialize_input
+from snakemakelib_workflows.scrnaseq import scrnaseq_qc
+from snakemakelib.bio.ngs.rnaseq.utils import read_gene_expression
+
+# Should be done first of all
+config["_samples"] = initialize_input(src_re = config['settings']['sample_organization'].raw_run_re,
+                                      sampleinfo = config['settings'].get('sampleinfo', None),
+                                      filter_suffix = config['bio.ngs.settings'].get("filter_suffix", ""),
+                                      sample_column_map = config['bio.ngs.settings'].get("sample_column_map", ""))
+
+def _merge_tx_suffix(aligner):
+    align_config = config['bio.ngs.align.' + aligner]
+    if aligner == "star":
+        return ".Aligned.toTranscriptome.out_unique.bam"
+
+def _find_transcript_bam(wildcards):
+    sources = make_targets(tgt_re=config['settings']['sample_organization'].run_id_re,
+                           samples = _samples,
+                           target_suffix = _merge_tx_suffix(config['bio.ngs.settings']['aligner']))
+    m = config['settings']['sample_organization'].sample_re.parse(wildcards.prefix)
+    sources = [src for src in sources if os.path.dirname(src).startswith(m['PATH'])]
+    return sources
+
+def find_scrnaseq_merge_inputs(wildcards):
+    """Find platform unit specific aligned bam files as input to picard
+    merge. 
+
+    NB: these are *not* the transcript-specific alignment files.
+    """
+    sources = make_targets(tgt_re = config['settings']['sample_organization'].run_id_re,
+                           samples = _samples,
+                           target_suffix = config['bio.ngs.settings']['aligner'])
+    m = config['settings']['sample_organization'].sample_re.parse(wildcards.prefix)
+    sources = [src for src in sources if os.path.dirname(src).startswith(m['PATH'])]
+    return sources
+
+
+# Configuration
+config_default = {
+    'settings' : {
+        'temporary_rules': [], #['star_align', 'bamtools_filter_unique'],
+        'protected_rules': [],
+    },
+    'workflows.bio.scrnaseq' : {
+        'qc' : {
+            # Add parameters here
+        },
+        'db' : {
+            'do_multo' : False,  ## Set to True to run generate multo database
+        },
+        'metadata' : None, ## Sample metadata
+        'report' : {
+            'directory' : 'report',
+        },
+    },
+    'bio.ngs.settings' : {
+        # Should be default for scrnaseq?
+        'aligner' : 'star',
+    },
+    'bio.ngs.qc.picard' : {
+        'merge_sam' : {
+            'label' : 'merge',
+            'inputfun' : find_scrnaseq_merge_inputs,
+        },
+    },
+    'bio.ngs.qc.rseqc' : {
+        'rules' : ['rseqc_qc_8'],
+    },
+    'bio.ngs.rnaseq.rpkmforgenes' : {
+        'rules' : ['rpkmforgenes_from_bam'],
+    },
+    'bio.ngs.rnaseq.rsem' : {
+        'rules' : ['rsem_calculate_expression', 'rsem_prepare_reference'],
+
+    },
+    'bio.ngs.align.star' : {
+        'rules' : ['star_align', 'star_align_se', 'star_index', 'star_align_log']
+    },
+}
+
+update_config(config_default, config)
+config = config_default
+
+aligner = config['bio.ngs.settings']['aligner']
+
+# Include necessary snakemakelib rules
+include: join(SNAKEMAKE_RULES_PATH, 'settings.rules')
+include: join(SNAKEMAKE_RULES_PATH, 'utils.rules')
+include: join(SNAKEMAKE_RULES_PATH, "bio/ngs/rnaseq", "rsem.rules")
+include: join(SNAKEMAKE_RULES_PATH, "bio/ngs/rnaseq", "rpkmforgenes.rules")
+align_include = join(SNAKEMAKE_RULES_PATH, "bio/ngs/align", aligner + ".rules")
+include: align_include
+include: join(SNAKEMAKE_RULES_PATH, "bio/ngs/tools", "bamtools.rules")
+include: join(SNAKEMAKE_RULES_PATH, "bio/ngs/qc", "rseqc.rules")
+include: join(SNAKEMAKE_RULES_PATH, "bio/ngs/qc", "picard.rules")
+include: join(SNAKEMAKE_RULES_PATH, "bio/ngs/tools", "samtools.rules")
+
+if aligner in ["bowtie", "bowtie2"]:
+    ruleorder: bamtools_filter_unique > picard_merge_sam > picard_sort_bam > bowtie_align
+
+if config['workflows.bio.scrnaseq']['db']['do_multo']:
+    include: join(SNAKEMAKE_RULES_PATH, "bio/ngs/tools", "multo.rules")
+    
+if workflow._workdir is None:
+    raise Exception("no workdir set, or set after include of 'scrnaseq.workflow'; set workdir before include statement!")
+
+# # Set temporary and protected outputs
+set_temporary_output(*[workflow.get_rule(x) for x in config['settings']['temporary_rules']])
+set_protected_output(*[workflow.get_rule(x) for x in config['settings']['protected_rules']])
+
+if config['bio.ngs.settings']['annotation']['transcript_annot_gtf']:
+    annotationstring = ".annotated"
+##################################################
+# Target definitions
+##################################################
+_samples = config["_samples"]
+if not config.get("samples", None)  is None:
+    # FIXME: samples should always be strings
+    _samples = [s for s in _samples if s["SM"] in config["samples"]]
+
+ALIGN_TARGETS = make_targets(tgt_re = config['settings']['sample_organization'].run_id_re,
+                                samples = _samples,
+                                target_suffix = ".Aligned.out.bam")
+
+ALIGN_LOG_TARGETS =  make_targets(tgt_re = config['settings']['sample_organization'].run_id_re,
+                                  samples = _samples,
+                                  target_suffix = ".Log.final.out")
+
+UNIQUE_TARGETS = [x.replace(".bam", "_unique.bam") for x in ALIGN_TARGETS]
+
+RSEQC_TARGETS = make_targets(tgt_re = config['settings']['sample_organization'].sample_re,
+                                samples = _samples,
+                                target_suffix = '.merge_rseqc/rseqc_qc_8.txt')
+RSEQC_GENE_BODY_COVERAGE = [x.replace("rseqc_qc_8.txt", "geneBody_coverage.geneBodyCoverage.txt") for x in RSEQC_TARGETS]
+RSEQC_READ_DISTRIBUTION = [x.replace("rseqc_qc_8.txt", "read_distribution.txt") for x in RSEQC_TARGETS]
+
+RPKMFORGENES_TARGETS = []
+if 'rpkmforgenes' in config['workflows.bio.scrnaseq']['quantification']:
+    RPKMFORGENES_TARGETS = make_targets(tgt_re = config['settings']['sample_organization'].sample_re,
+                                        samples = _samples,
+                                        target_suffix = '.merge.rpkmforgenes')
+
+
+RSEM_TARGETS = []
+if 'rsem' in config['workflows.bio.scrnaseq']['quantification']:
+    RSEM_TARGETS = make_targets(tgt_re = config['settings']['sample_organization'].sample_re,
+                                samples = _samples,
+                                target_suffix = '.merge.tx.genes.results')
+
+REPORT=config['workflows.bio.scrnaseq']['report']['directory']
+REPORT_TARGETS = ['{report}/scrnaseq_summary.html'.format(report=REPORT)] +\
+                 ['{report}/star.Aligned.out.csv'.format(report=REPORT)] +\
+                 ['{report}/rpkmforgenes.merge{annot}.csv'.format(report=REPORT, annot=annotationstring)] +\
+                 ['{report}/rsem.merge.tx.genes{annot}.csv'.format(report=REPORT, annot=annotationstring),
+                  '{report}/rsem.merge.tx.isoforms{annot}.csv'.format(report=REPORT, annot=annotationstring)]
+
+# Utility rules - rely on targets so must be placed last
+include: "./utils.rules"
+
+# Workflow rules
+rule scrnaseq_all:
+    """Run scRNAseq pipeline"""
+    input: UNIQUE_TARGETS + RSEQC_TARGETS + RPKMFORGENES_TARGETS + RSEM_TARGETS + REPORT_TARGETS
+
+rule scrnaseq_align:
+    """Run alignments"""
+    input: ALIGN_TARGETS
+
+rule scrnaseq_unique:
+    """Generate uniquely mapping output"""
+    input: UNIQUE_TARGETS
+
+rule scrnaseq_rseqc:
+    """Run RSeQC"""
+    input: RSEQC_TARGETS
+
+rule scrnaseq_rpkmforgenes:
+    """Run rpkmforgenes"""
+    input: RPKMFORGENES_TARGETS
+
+rule scrnaseq_rsem:
+    """Run rpkmforgenes"""
+    input: RSEM_TARGETS
+    
+rule scrnaseq_targets:
+    """Print targets """
+    run:
+        print (ALIGN_TARGETS)
+        print (RSEQC_TARGETS)
+        print (RPKMFORGENES_TARGETS)
+        print (RSEM_TARGETS)
+
+rule scrnaseq_clean:
+    """Clean working directory. WARNING: will remove all files except
+    (.fastq|.fastq.gz) and csv files
+    """
+    params: d = workflow._workdir
+    shell: 'for f in `find  {params.d} -type f -name "*" | grep -v ".fastq$" | grep -v ".fastq.gz$" | grep -v ".csv$"`; do echo removing $f; rm -f $f; done'
+
+
