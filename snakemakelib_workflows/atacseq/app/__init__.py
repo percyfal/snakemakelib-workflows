109875e6a41a8829a269c933491be3d7c16880d5
Per Unneberg <per.unneberg@scilifelab.se>
2016-10-12 16:34:50 +0200

    Delete source code

--- snakemakelib_workflows/atacseq/app/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (C) 2015 by Per Unneberg
-from __future__ import absolute_import
-
-from .graphics import *
-from .results import *
-from .hooks import *
--- snakemakelib_workflows/scrnaseq/app/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2015 by Per Unneberg
-
-from .pca import *
-from .technicalnoise import *
-from .graphics import *
-from .report import *
-from .hooks import *
-from .utils import *
